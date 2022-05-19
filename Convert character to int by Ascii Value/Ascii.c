99fbcbdd7a6f7179a388d5285f3f3bc9d0da48e5
Jewel Talukder <97118351+jewel-talukder@users.noreply.github.com>
2022-05-20 01:03:49 +0600

    Add files via upload

---
+++ Convert character to int by Ascii Value/Ascii.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a Character Value :  ");
+    scanf("%c",&ch);
+    printf("Convert Character  To  Interger  by Ascii value :  %d\n",ch);
+
+
+
+}
