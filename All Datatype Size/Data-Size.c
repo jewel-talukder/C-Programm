c62a800ce6db64696b396d58e0d30b7a81040651
Jewel Talukder <97118351+jewel-talukder@users.noreply.github.com>
2022-05-20 01:05:57 +0600

    Add files via upload

---
+++ All Datatype Size/Data-Size.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+int i;
+float f;
+double b;
+char c;
+printf("Size of Integer Number : %d Bytes \n",sizeof(i));
+printf("Size of Float Number : %d Bytes \n",sizeof(f));
+printf("Size of Double Number : %d Bytes \n",sizeof(b));
+printf("Size of Character Number : %d Bytes \n",sizeof(c));
+}
