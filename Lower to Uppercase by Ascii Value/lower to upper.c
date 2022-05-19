ac9f42342bcff184a09013613b50a0866e123f9d
Jewel Talukder <97118351+jewel-talukder@users.noreply.github.com>
2022-05-20 02:09:01 +0600

    Add files via upload

---
+++ Lower to Uppercase by Ascii Value/lower to upper.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    char lower;
+    printf("Enter a Lowercase Letter : ");
+    scanf("%c",&lower);
+    printf("The Uppercase Letter = %c ",lower -32);
+    return 0;
+
+
+}
