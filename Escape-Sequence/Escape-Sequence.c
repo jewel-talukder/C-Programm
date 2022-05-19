313a29f780e3f2298641266227d8a12b0f7d783e
Jewel Talukder <97118351+jewel-talukder@users.noreply.github.com>
2022-05-20 01:06:35 +0600

    Add files via upload

---
+++ Escape-Sequence/Escape-Sequence.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    printf("Dont Show the value");
+    printf("All most work Delete Before Not After    \r");
+  printf("\aThis is jewel talukder\n");
+  printf("I am a Programmer in C#  \f  \n ");
+  printf("I am a Student \b   fhtukrkukiu \n");
+  printf("This is my Programm\e");
+    printf(" \nThis Me For tot tot\t\t Again\n ");
+  printf("This is T-Shirt \v  \n ");
+  printf("  \\What a programm\\\n");
+  printf("   \' There is a man that ???  \'  \n ");
+  printf("  \" Where are you from man??\"  \n  ");
+  printf("  \\\?This is my First Programm in C\?\\\n");
+  printf(" Using NameSpace\n");
+  printf("Who are You\?\?\?");
+ getchar();
+}
