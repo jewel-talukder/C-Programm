2c2309f7ec1c7fe721ddb22ec1d72284eb605cd3
Jewel Talukder <97118351+jewel-talukder@users.noreply.github.com>
2022-05-20 11:49:12 +0600

    Add files via upload

---
+++ Hexa to Decimal Number/Hexa to Decimal.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int Hexa;
+    printf("Enter a Hexadecimal Number : ");
+    scanf("%x",&Hexa);
+    printf("The Decimal Numbe is = %d ",Hexa);
+    return 0;
+}
