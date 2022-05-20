bad6ddd7854cd11f5b07b32c3468e5953bd12d06
Jewel Talukder <97118351+jewel-talukder@users.noreply.github.com>
2022-05-20 11:50:11 +0600

    Add files via upload

---
+++ Octal to Hexa/octal to hexa.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int octal;
+    printf("Enter a Octal Number : ");
+    scanf("%o",&octal);
+    printf("The HexaDecimal Number is = %x" , octal);
+    return 0;
+}
