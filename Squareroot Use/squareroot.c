6e1b631369924757862c6f991814c4ced4287735
Jewel Talukder <97118351+jewel-talukder@users.noreply.github.com>
2022-05-25 23:09:04 +0600

    Add files via upload

---
+++ Squareroot Use/squareroot.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    double result;
+    printf("Enter A value : ");
+    scanf("%d",&a);
+   result =  sqrt(a);
+    printf(" Square root = %.2lf",result);
+    return 0;
+}
+
